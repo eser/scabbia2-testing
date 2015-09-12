6f97897ad6214ad1e59c03e74436d8d2358f1934
Eser Ozvataf <eser@ozvataf.com>
2015-09-13 01:43:27 +0300

    * some urls in descriptions are updated.

--- src/Testing/Testing.php
+++ src/Testing/Testing.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Scabbia2 PHP Framework Code
+ * Scabbia2 Testing Component
  * http://www.scabbiafw.com/
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @link        http://github.com/scabbiafw/scabbia2-fw for the canonical source repository
+ * @link        https://github.com/scabbiafw/scabbia2-testing for the canonical source repository
  * @copyright   2010-2015 Scabbia Framework Organization. (http://www.scabbiafw.com/)
  * @license     http://www.apache.org/licenses/LICENSE-2.0 - Apache License, Version 2.0
  */
@@ -23,7 +23,7 @@ use Scabbia\Interfaces\IInterface;
  * Scabbia2 PHP Framework's itself and related production code
  *
  * @package     Scabbia\Testing
- * @author      Eser Ozvataf <eser@sent.com>
+ * @author      Eser Ozvataf <eser@ozvataf.com>
  * @since       2.0.0
  */
 class Testing
